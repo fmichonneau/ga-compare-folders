8675d7161741cb36d59786903587e2139da90f0b
François Michonneau <francois.michonneau@gmail.com>
2021-11-01 11:15:21 -0400

    use correct syntax for full invalidation

--- compare-folders.R
+++ compare-folders.R
@@ -35,11 +35,11 @@ ga_compare_folders <- function(path_1, path_2, out, add_roots = c("true", "false
   }
 
   ## the file that can be used to specify the paths to invalidate cannot
-  ## be more than 4000 characters. If it's the case, we only write `*`
+  ## be more than 4000 characters. If it's the case, we only write `"/*"`
   ## to the file to invalidate everything.
   if (sum(nchar(res)) + length(res) >=  4000) {
     message("too many files to invalidate, default on everything.")
-    cat("*", file = out)
+    cat("\\"/*\\"", file = out)
     return(res)
   }
   
